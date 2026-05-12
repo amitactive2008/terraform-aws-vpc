063685879d836128f607658394f6f0ad0f275f6b
Amit Kumar <amit.active2008@gmail.com>
2026-05-12 21:28:24 +0530

    TF vpc Module add

---
+++ output.tf
@@ -0,0 +1,5 @@
+output "vpc_id" {
+    description = "vpc_id"
+    value = aws_vpc.main.id
+  
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    default = "us-east-1"
+  
+}
+
+variable "cidr_block" {
+    default = "10.0.0.0/16"
+  
+}
+
+variable "env" {
+    default = "dev"
+  
+}
